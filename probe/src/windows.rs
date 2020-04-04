14a0b0d43bc8d4422d290ad9e8584b1c087affae
lqd <remy.rakic+github@gmail.com>
2020-04-04 15:25:04 +0200

    windows: query memory information

--- probe/src/windows.rs
+++ probe/src/windows.rs
@@ -2,12 +2,15 @@ use std::time::Duration;
 
 use winapi::shared::minwindef::FILETIME;
 use winapi::um::processthreadsapi::{GetCurrentProcess, GetProcessTimes};
+use winapi::um::psapi::{GetProcessMemoryInfo, PROCESS_MEMORY_COUNTERS_EX};
 
 use super::{OsReadings, ReadingsResult};
 
 pub(crate) fn get_os_readings() -> ReadingsResult<OsReadings> {
     unsafe {
         let h_process = GetCurrentProcess();
+
+        // Query timers information
         let mut creation_time: FILETIME = std::mem::zeroed();
         let mut exit_time: FILETIME = std::mem::zeroed();
         let mut kernel_time: FILETIME = std::mem::zeroed();
@@ -26,14 +29,27 @@ pub(crate) fn get_os_readings() -> ReadingsResult<OsReadings> {
             (user_time.dwHighDateTime as u32 as u64) << 32 + user_time.dwLowDateTime as u64;
         let user_time = Duration::from_nanos(user_time * 100);
 
+        // Query memory information
+        let mut mem_counters: PROCESS_MEMORY_COUNTERS_EX = std::mem::zeroed();
+        GetProcessMemoryInfo(
+            h_process,
+            &mut mem_counters as *mut PROCESS_MEMORY_COUNTERS_EX as *mut _,
+            std::mem::size_of::<PROCESS_MEMORY_COUNTERS_EX>() as u32,
+        );
+
+        let virtual_size = mem_counters.PrivateUsage as u64;
+        let resident_size = mem_counters.WorkingSetSize as u64;
+        let resident_size_max = mem_counters.PeakWorkingSetSize as u64;
+        let major_fault = mem_counters.PageFaultCount as u64;
+
         let usage = OsReadings {
-            virtual_size: 0,
-            resident_size: 0,
-            resident_size_max: 0,
+            virtual_size,
+            resident_size,
+            resident_size_max,
             user_time,
             system_time,
             minor_fault: 0,
-            major_fault: 0,
+            major_fault,
         };
 
         Ok(usage)
