ef5af7add9f22b2d65dee7e3445052e9c922d78f
Mathieu Poumeyrol <kali@zoy.org>
2021-11-24 13:57:01 +0100

    move allocator instrumentation out of the way

---
+++ probe/src/alloc.rs
@@ -0,0 +1,55 @@
+use std::sync::atomic::AtomicUsize;
+
+pub static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+pub static FREEED: AtomicUsize = AtomicUsize::new(0);
+
+/// Setup global allocator instrumentation, to track rust-managed memory.
+///
+/// It is not mandatory to do so, as we also register the RSZ and VSZ as
+/// reported by the OS, but it may be interesting. From our experience it may be
+/// worth activating it as the cost is relatively modest.
+///
+/// This macro allows to specify a specific allocator instance to wrap and make global
+/// (for instance, jemalloc).
+#[macro_export]
+macro_rules! wrap_global_allocator {
+    ($alloc:path) => {
+        #[global_allocator]
+        static A: InstrumentedAllocator = InstrumentedAllocator;
+
+        struct InstrumentedAllocator;
+
+        unsafe impl std::alloc::GlobalAlloc for InstrumentedAllocator {
+            unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+                use std::sync::atomic::Ordering::Relaxed;
+                let ptr = $alloc.alloc(layout);
+                if !ptr.is_null() {
+                    $crate::alloc::ALLOCATED.fetch_add(layout.size(), Relaxed);
+                }
+                ptr
+            }
+            unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+                use std::sync::atomic::Ordering::Relaxed;
+                if !ptr.is_null() {
+                    $crate::alloc::FREEED.fetch_add(layout.size(), Relaxed);
+                }
+                $alloc.dealloc(ptr, layout);
+            }
+        }
+    };
+}
+
+/// Setup global allocator instrumentation, to track rust-managed memory.
+///
+/// It is not mandatory to do so, as we also register the RSZ and VSZ as
+/// reported by the OS, but it may be interesting. From our experience it may be
+/// worth activating it as the cost is relatively modest.
+///
+/// This macro wrap the System allocator. Use `wrap_global_allocator` to wrap
+/// another global allocator.
+#[macro_export]
+macro_rules! instrumented_allocator {
+    () => {
+        $crate::wrap_global_allocator!(std::alloc::System);
+    };
+}
